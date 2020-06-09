2cebe6f92bbcdea9abc00f89322fc4f93a9eef39
向玮鑫 <xiangweixin.east@bytedance.com>
2020-06-09 18:40:51 +0800

    图片渲染CENTER_INSIDE实现

--- app/src/main/java/com/xiangweixin/myownstudy/opengl/ImageSurfaceViewRender.java
+++ app/src/main/java/com/xiangweixin/myownstudy/opengl/ImageSurfaceViewRender.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.xiangweixin.myownstudy.util.LogUtil;
 
@@ -23,9 +24,10 @@ public class ImageSurfaceViewRender implements GLSurfaceView.Renderer {
             "attribute vec4 a_position;\n" +
             "attribute vec2 a_texturePosition;\n" +
             "varying vec2 v_texturePosition;\n" +
+            "uniform mat4 mvpMatrix;\n" +
             "void main() {\n" +
             "    v_texturePosition = a_texturePosition;\n" +
-            "    gl_Position = a_position;\n" +
+            "    gl_Position = mvpMatrix * a_position;\n" +
             "}";
     private static final String fragmentShaderCode = "precision mediump float;\n" +
             "varying vec2 v_texturePosition;\n" +
@@ -51,6 +53,11 @@ public class ImageSurfaceViewRender implements GLSurfaceView.Renderer {
     private int texID = -1;
     private int programID = -1;
 
+    private int imageWidth = 0;
+    private int imageHeight = 0;
+    private int viewWitdh = 0;
+    private int viewHeight = 0;
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         initData();
@@ -61,13 +68,30 @@ public class ImageSurfaceViewRender implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        viewWitdh = width;
+        viewHeight = height;
         GLES20.glViewport(0, 0, width, height);
         LogUtil.formatI(TAG, "glViewPort >>> x: %d, y: %d, width: %d, height: %d", 0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        drawWithProgram(programID, vertexData, textureData, texID);
+        float[] matrix = new float[16];
+        Matrix.setIdentityM(matrix, 0);
+        /**
+         * 需要注意缩放比例对应的不是原图宽高，而是相对于被glViewPort(w, h)强行采样为w * h的图像的比例
+         * CENTER_INSIDE
+         */
+        float imageRatio = imageWidth * 1.0f / imageHeight;
+        float viewRatio = viewWitdh * 1.0f / viewHeight;
+        if (imageRatio > viewRatio) {
+            float scaleH = imageHeight * viewWitdh * 1.0f / (viewHeight * imageWidth);
+            Matrix.scaleM(matrix, 0, 1f, scaleH, 1F);
+        } else {
+            float scaleW = imageWidth * viewHeight * 1.0f / (imageHeight * viewHeight);
+            Matrix.scaleM(matrix, 0, scaleW, 1F, 1F);
+        }
+        drawWithProgram(programID, vertexData, textureData, texID, matrix);
     }
 
     private void initData() {
@@ -95,6 +119,8 @@ public class ImageSurfaceViewRender implements GLSurfaceView.Renderer {
         ByteBuffer buffer = ByteBuffer.allocate(bitmap.getWidth() * bitmap.getHeight() * 4);
         bitmap.copyPixelsToBuffer(buffer);
         buffer.position(0);
+        imageWidth = bitmap.getWidth();
+        imageHeight = bitmap.getHeight();
 
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureID);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
@@ -156,7 +182,7 @@ public class ImageSurfaceViewRender implements GLSurfaceView.Renderer {
         return program;
     }
 
-    private void drawWithProgram(int programID, FloatBuffer vertexPos, FloatBuffer fragPos, int texture) {
+    private void drawWithProgram(int programID, FloatBuffer vertexPos, FloatBuffer fragPos, int texture, float[] matrix) {
         GLES20.glUseProgram(programID);
 
         int aPositionLocation = GLES20.glGetAttribLocation(programID, "a_position");
@@ -170,6 +196,9 @@ public class ImageSurfaceViewRender implements GLSurfaceView.Renderer {
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture);
         int uTexturePos = GLES20.glGetUniformLocation(programID, "u_texture");
         GLES20.glUniform1f(uTexturePos, 0);//默认纹理单元为0
+
+        int mvpLocation = GLES20.glGetUniformLocation(programID, "mvpMatrix");
+        GLES20.glUniformMatrix4fv(mvpLocation, 1, false, matrix, 0);
 
         //draw.
         GLES20.glClearColor(0, 0, 0, 1);
---
+++ app/src/main/java/com/xiangweixin/myownstudy/opengl/ReshapeUtils.java
@@ -0,0 +1,7 @@
+package com.xiangweixin.myownstudy.opengl;
+
+/**
+ * 用于对纹理的旋转、裁剪、镜像
+ */
+public class ReshapeUtils {
+}
